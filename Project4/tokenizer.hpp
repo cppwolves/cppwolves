f2a5fb6d2f03fcf0f9b5d8604ab8c175eb4aa9d1
Joel Thomas <jlthomas.e@gmail.com>
2024-10-27 09:12:17 -0700

    Somehow tokenizer was using a old version

--- Project4/tokenizer.hpp
+++ Project4/tokenizer.hpp
@@ -2,42 +2,43 @@
 #ifndef TOKENIZER_HPP
 #define TOKENIZER_HPP
 
-#include <string>
-#include <vector>
+#include "token.hpp"
 #include <fstream>
 #include <queue>
-#include "token.hpp"
+#include <string>
+#include <vector>
 
 class Tokenizer {
 public:
-    Tokenizer(const std::string& filename);
-    std::vector<Token> tokenize();
-    std::string errorMessage;
-    std::queue<Token> tokenQueue;
+  Tokenizer(const std::string &filename);
+  std::vector<Token> tokenize();
+  std::string errorMessage;
+  std::queue<Token> tokenQueue;
 
 private:
-    std::ifstream inputFile;
-    int lineNumber;
-    char currentChar;
-    bool endOfFile;
+  std::vector<Token> tokens;
+  std::ifstream inputFile;
+  int lineNumber;
+  char currentChar;
+  bool endOfFile;
 
-    // Helper methods
-    void advance();
-    void skipWhitespace();
-    void skipComment();
+  // Helper methods
+  void advance();
+  void skipWhitespace();
+  void skipComment();
 
-    Token getNextToken();
-    Token identifierOrKeyword();
-    Token number();
-    Token stringLiteral();
-    Token charLiteral();
-    Token operatorOrDelimiter();
+  Token getNextToken();
+  Token identifierOrKeyword();
+  Token number();
+  Token stringLiteral();
+  Token charLiteral();
+  Token operatorOrDelimiter();
 
-    bool isKeyword(const std::string& str);
-    bool isLetter(char ch);
-    bool isDigit(char ch);
-    bool isHexDigit(char ch);
-    bool isOperatorChar(char ch);
+  bool isKeyword(const std::string &str);
+  bool isLetter(char ch);
+  bool isDigit(char ch);
+  bool isHexDigit(char ch);
+  bool isOperatorChar(char ch);
 };
 
 #endif // TOKENIZER_HPP
